20c03c2c107ec5b3d08598e2d74a20db6ad52f46
aswinkrishna <newuser@aswinkrishnas-MacBook-Air.local>
2025-10-17 15:56:48 +0530

    Update layout, JS scroll-shadow logic, and new images

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".buttonActive-1");
+
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+     
+      buttons.forEach(btn => btn.classList.remove("active"));
+
+      
+      button.classList.add("active");
+    });
+  });
+});
+
